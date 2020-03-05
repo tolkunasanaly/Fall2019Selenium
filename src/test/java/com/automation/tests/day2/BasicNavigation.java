38dbe0e86509f9487fdc708792d15cfd41f79ea2
tolkunasanaly <tolkusha@gmail.com>
2020-03-05 17:10:55 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -16,6 +16,8 @@ public class BasicNavigation {
         // in Selenium everything starts from WebDriver interface
         //ChromeDriver extends RemoteWebDriver --> implements WebDriver
         driver.get("http://google.com"); //to open a website
+        driver.manage().window().maximize(); //to maximize browser
+      //  driver.manage().window().fullscreen();
 
         Thread.sleep(3000); //for demo, wait 3 seconds
         //method that return page title
@@ -29,9 +31,25 @@ public class BasicNavigation {
         }else{
             System.out.println("TEST FAILED!");
         }
+        //go to another website within the same window
+        driver.navigate().to("htpp://amazon.com");
+
+        if (driver.getTitle().toLowerCase().contains("amazon")){
+            System.out.println("TEST PASSED!");
+        }else{
+            System.out.println("TEST FAILED!");
+        }
 
         //must be at the end
         driver.close(); // to close browser
         //browser cannot close itself
+    }
+
+    public static void verifyEquals(String arg1, String arg2){
+        if (arg1.equals(arg2)){
+            System.out.println("TEST PASSED!");
+        }else{
+            System.out.println("TEST FAILED!");
+        }
     }
 }
