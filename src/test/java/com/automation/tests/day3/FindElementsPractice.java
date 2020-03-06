874bb0617eef98661f3bd32a95437d866ec619af
tolkunasanaly <tolkusha@gmail.com>
2020-03-06 15:49:42 -0500

    finding elements

--- src/test/java/com/automation/tests/day3/FindElementsPractice.java
+++ src/test/java/com/automation/tests/day3/FindElementsPractice.java
@@ -2,26 +2,38 @@ package com.automation.tests.day3;
 
 import com.automation.utilities.DriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FindElementsPractice {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
 //        WebDriverManager.chromedriver().setup();
 //        WebDriver driver = new ChromeDriver();
 
         WebDriver driver = DriverFactory.createADriver("chrome");
         driver.get("http://practice.cybertekschool.com/sign_up");
+        WebElement fullName = driver.findElement(By.name("full_name"));
+        fullName.sendKeys("Mister Twister");
+
+        Thread.sleep(2000);
+
+        WebElement email = driver.findElement(By.name("email"));
+        email.sendKeys("sdet@cybertek.com");
+
+        Thread.sleep(2000);
 
 
+        WebElement signUp = driver.findElement(By.name("wooden_spooon"));
+     //   signUp.submit();
+        signUp.click();
 
+        Thread.sleep(2000);
 
         driver.quit(); // to close everything
-
-
-
 
     }
 }
