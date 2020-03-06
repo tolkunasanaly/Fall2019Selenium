0b88eb735394248e3f463cab8807384852ecd384
tolkunasanaly <tolkusha@gmail.com>
2020-03-06 15:10:58 -0500

    finding elements

--- src/test/java/com/automation/tests/day3/FindElementsPractice.java
+++ src/test/java/com/automation/tests/day3/FindElementsPractice.java
@@ -1,5 +1,6 @@
 package com.automation.tests.day3;
 
+import com.automation.utilities.DriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,8 +9,18 @@ public class FindElementsPractice {
 
     public static void main(String[] args) {
 
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+//        WebDriverManager.chromedriver().setup();
+//        WebDriver driver = new ChromeDriver();
+
+        WebDriver driver = DriverFactory.createADriver("chrome");
+        driver.get("http://practice.cybertekschool.com/sign_up");
+
+
+
+
+        driver.quit(); // to close everything
+
+
 
 
     }
