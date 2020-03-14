974d759bb49183a27256480e86a54a55a57a9a1e
tolkunasanaly <tolkusha@gmail.com>
2020-03-14 13:18:09 -0400

    Buttons

--- src/test/java/com/automation/tests/day8/BasicTestNGTests.java
+++ src/test/java/com/automation/tests/day8/BasicTestNGTests.java
@@ -1,22 +1,30 @@
 package com.automation.tests.day8;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class BasicTestNGTests {
+    @BeforeMethod
+    public void setup(){
+        System.out.println("Before Method");
+    }
 
     @Test
-    public void test1(){
-        String expected ="apple";
+    public void test1() {
+        System.out.println("TEST 1");
+        String expected = "apple";
         String actual = "apple";
-
         Assert.assertEquals(actual, expected);
     }
 
     @Test
-    public void test2(){
+    public void test2() {
+        System.out.println("TEST 2");
         int num1 = 5;
-        int num2 =10;
-        Assert.assertTrue(num1>num2);
+        int num2 = 10;
+        //it calls hard assertion.
+        //if assertion fails - it stops the execution (due to exception).
+        Assert.assertTrue(num1 > num2);
     }
 }
--- src/test/java/com/automation/tests/day8/UnitTestPractice.java
+++ src/test/java/com/automation/tests/day8/UnitTestPractice.java
@@ -6,32 +6,40 @@ import org.testng.annotations.Test;
 public class UnitTestPractice {
 
     public static void main(String[] args) {
+        //unit test
+        //to check if our method works properly
+        //if assertion fails, that means our method doesn't work correctly
+        //that means we have fix the method
         String expected = "cba";
         String toReverse = "abc";
         String actual = reverseString(toReverse);
+        //Assertion
+        verifyEquals(expected, actual);
+
     }
+
     //annotation
-    @Test (description = "Verify if method can reverse a string")
+    //description - is not working for junit, make sure that you use testng
+    @Test(description = "Verify if method can reverse a string")
     public void test(){
-        String expected ="elppa";
+        String expected = "elpp";
         String actual = reverseString("apple");
-
+        //it coming from testng, junit also has this class
+        //you can compare any data types here: strings, primitives, arrays, objects
+        //to verify if expected result is equals to actual
         Assert.assertEquals(actual, expected);
-
     }
 
-    @Test (description = "Verify if method can reverse a string")
+    @Test(description = "Verify if method can reverse a string")
     public void test2(){
-        String expected = " rac";
-        String actual =reverseString("car");
+        String expected = "rac";
+        String actual = reverseString("car");
         Assert.assertEquals(actual, expected);
-
     }
 
 
 
-
-    public boolean verifyEquals(String expected, String actual) {
+    public static boolean verifyEquals(String expected, String actual) {
         if (expected.equals(actual)) {
             System.out.println("TEST PASSED");
             return true;
@@ -42,6 +50,7 @@ public class UnitTestPractice {
             return false;
         }
     }
+
     /**
      * This method stands for reversing strings.
      *
