13f777af35a4bb81452efad60be97e8a03380631
tolkunasanaly <tolkusha@gmail.com>
2020-03-24 20:53:29 -0400

    tests

--- src/test/java/com/automation/tests/day11/WebTables.java
+++ src/test/java/com/automation/tests/day11/WebTables.java
@@ -1,5 +1,4 @@
 package com.automation.tests.day11;
-
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -67,11 +66,74 @@ public class WebTables {
      * To get specific column, skip row index, and just provide td index
      */
     @Test
-    public void getSpecificColumn(){
+    public void getSpecificColumn() {
         //td[5] - column with links
         List<WebElement> links = driver.findElements(By.xpath("//table[1]//tbody//tr//td[5]"));
         System.out.println(BrowserUtils.getTextFromWebElements(links));
     }
 
-}
+    /**
+     * Go to tables example page
+     * Delete record with jsmith@gmail.com email
+     * verify that number of rows is equals to 3
+     * verify that jsmith@gmail.com doesn't exists any more in the table
+     */
 
+    @Test
+    public void deleteRowTest() {
+        String xpath = "//table[1]//td[text()='jsmith@gmail.com']/..//a[text()='delete']";
+        driver.findElement(By.xpath(xpath)).click();
+
+        BrowserUtils.wait(3);
+
+        //get count of rows
+        int rowCount = driver.findElements(By.xpath("//table[1]//tbody//tr")).size();
+
+        Assert.assertEquals(rowCount, 3);
+
+        List<WebElement> emails = driver.findElements(By.xpath("//table[1]//td[text()='jsmith@gmail.com']"));
+
+        Assert.assertTrue(emails.isEmpty());
+    }
+
+    /**
+     * Let's write a function that will return column index based on column name
+     */
+    @Test
+    public void getColumnIndexByName() {
+        String columnName = "Email";
+
+        List<WebElement> columnNames = driver.findElements(By.xpath("//table[1]//th"));
+
+        int index = 0;
+        for (int i = 0; i < columnNames.size(); i++) {
+            String actualColumnName = columnNames.get(i).getText();
+
+            System.out.println(String.format("Column name: %s, position %s", actualColumnName, i));
+
+            if (actualColumnName.equals(columnName)) {
+                index = i + 1;
+                break;
+            }
+        }
+
+        Assert.assertEquals(index, 3);
+
+    }
+
+    @Test
+    public void getSpecificCell() {
+        String expected = "http://www.jdoe.com";
+
+        int row = 3;
+        int column = 5;
+
+        String xpath = "//table[1]//tbody//tr[" + row + "]//td[" + column + "]";
+        WebElement cell = driver.findElement(By.xpath(xpath));
+
+        Assert.assertEquals(cell.getText(), expected);
+
+    }
+
+
+}
---
+++ src/test/java/com/automation/tests/day13/SystemProperties.java
@@ -0,0 +1,11 @@
+package com.automation.tests.day13;
+
+import org.testng.annotations.Test;
+
+public class SystemProperties {
+
+    @Test
+    public void test(){
+        System.out.println(System.getProperty("user.dir"));
+    }
+}
