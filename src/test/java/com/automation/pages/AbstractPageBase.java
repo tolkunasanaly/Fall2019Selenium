3077184bfefb5735dba616250c21a620b02d300e
tolkunasanaly <tolkusha@gmail.com>
2020-03-28 16:35:36 -0400

    tests

---
+++ src/test/java/com/automation/pages/AbstractPageBase.java
@@ -0,0 +1,45 @@
+package com.automation.pages;
+
+import com.automation.utilities.BrowserUtils;
+import com.automation.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * This class will be extended by page classes
+ * Ant common webelements/locators can be stored here
+ * Since navigation menu doesn't belong to particular page
+ * We cannot really create a dedicated page class to store
+ * elements from that menu
+ */
+public abstract class AbstractPageBase {
+    protected WebDriver driver = Driver.getDriver();
+
+    public AbstractPageBase(){
+        PageFactory.initElements(driver, this);
+    }
+
+    public void navigateTo(String tabName, String moduleName){
+        String tabNameXpath = "//span[@class='title title-level-1' and contains(text(),'"+tabName+"')]";
+        String moduleXpath = "//span[@class='title title-level-2' and text(),'"+moduleName+"']";
+
+
+        WebElement tabElement = driver.findElement(By.xpath(tabNameXpath));
+        WebElement moduleElement = driver.findElement(By.xpath(moduleXpath));
+
+        Actions actions = new Actions(driver);
+
+        BrowserUtils.wait(4);
+
+        actions.moveToElement(tabElement).
+                pause(2000).
+                click(moduleElement).
+                build().perform();
+
+
+    }
+
+}
--- src/test/java/com/automation/pages/LoginPage.java
+++ src/test/java/com/automation/pages/LoginPage.java
@@ -1,55 +1,64 @@
 package com.automation.pages;
 
+import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.ConfigurationReader;
 import com.automation.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import java.util.ConcurrentModificationException;
-
 public class LoginPage {
 
     @FindBy(id = "prependedInput")
-    public WebElement username;
+    private WebElement username;
+//    public WebElement username2 = Driver.getDriver().findElement(By.id("prependedInput"));
 
     @FindBy(id = "prependedInput2")
-    public WebElement password;
+    private WebElement password;
 
     @FindBy(id = "_submit")
-    public WebElement login;
+    private WebElement login;
 
     @FindBy(linkText = "Forgot your password?")
-    public WebElement forgotPassword;
+    private WebElement forgotPassword;
 
+    @FindBy(css = "[class='alert alert-error']")
+    private WebElement warningMessage;
 
-    public LoginPage(){
+    public LoginPage() {
         //to connect our webdriver, page class and page factory
-        //pageFactory = used to use @FindBy annotations
-        //PageFactory = helps to find elements easier
+        //PageFactory - used to use @FindBy annotations
+        //PageFactory - helps to find elements easier
         PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    public String getWarningMessageText() {
+        return warningMessage.getText();
     }
 
     /**
      * Method to login, version #1
+     * Login as a specific user
+     *
      * @param usernameValue
-     * @param passwordVAlue
+     * @param passwordValue
      */
-    public void login(String usernameValue, String passwordVAlue){
+    public void login(String usernameValue, String passwordValue) {
         username.sendKeys(usernameValue);
-        password.sendKeys(passwordVAlue);
+        password.sendKeys(passwordValue, Keys.ENTER);
+        BrowserUtils.wait(3);
     }
 
     /**
      * Method to login, version #2
-     * Login as
+     * Login as a default user
      * Credentials will be retrieved from configuration.properties file
      */
-    public void login(){
+    public void login() {
         username.sendKeys(ConfigurationReader.getProperty("store_manager"));
         password.sendKeys(ConfigurationReader.getProperty("password"), Keys.ENTER);
-
-
+        BrowserUtils.wait(3);
     }
 }
