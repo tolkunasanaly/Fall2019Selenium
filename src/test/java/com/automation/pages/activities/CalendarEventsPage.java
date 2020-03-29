9011da2e8632241c5b457ad55e761249994b580d
tolkunasanaly <tolkusha@gmail.com>
2020-03-29 12:17:20 -0400

    tests

---
+++ src/test/java/com/automation/pages/activities/CalendarEventsPage.java
@@ -0,0 +1,18 @@
+package com.automation.pages.activities;
+
+import com.automation.pages.AbstractPageBase;
+import com.automation.utilities.BrowserUtils;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class CalendarEventsPage extends AbstractPageBase {
+
+    @FindBy(css = "[title='Create Calendar event']")
+    private WebElement createCalendarEvent;
+
+    public void clickToCreateCalendarEvent() {
+        BrowserUtils.waitForPageToLoad(10);
+        wait.until(ExpectedConditions.elementToBeClickable(createCalendarEvent)).click();
+    }
+}
