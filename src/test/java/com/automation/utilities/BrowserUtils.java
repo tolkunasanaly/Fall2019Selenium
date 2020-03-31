33bd2b7d0dbe422fa36edfc0f0caa395e69eedd6
tolkunasanaly <tolkusha@gmail.com>
2020-03-31 19:48:45 -0400

    tests

--- src/test/java/com/automation/utilities/BrowserUtils.java
+++ src/test/java/com/automation/utilities/BrowserUtils.java
@@ -1,10 +1,15 @@
 package com.automation.utilities;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,8 +27,8 @@ public class BrowserUtils {
             e.printStackTrace();
         }
     }
+
     /**
-     *
      * @param elements represents collection of WebElements
      * @return collection of strings
      */
@@ -34,6 +39,7 @@ public class BrowserUtils {
         }
         return textValues;
     }
+
     /**
      * waits for backgrounds processes on the browser to complete
      *
@@ -48,6 +54,7 @@ public class BrowserUtils {
             error.printStackTrace();
         }
     }
+
     /**
      * Clicks on an element using JavaScript
      *
@@ -65,6 +72,20 @@ public class BrowserUtils {
      */
     public static void scrollTo(WebElement element) {
         ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    public static String getScreenshot(String name) {
+        String path = System.getProperty("user.dir") + "/test-output/screenshot/" + name + ".png";
+        System.out.println("Screenshot is here: "+ path);
+        TakesScreenshot takesScreenshot = (TakesScreenshot) Driver.getDriver();
+        File source = takesScreenshot.getScreenshotAs(OutputType.FILE);
+        File destination = new File(path);
+        try {
+            FileUtils.copyFile(source, destination);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return path;
     }
 }
 
