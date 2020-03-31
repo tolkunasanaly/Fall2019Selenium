da4f7d500182ea1b93f23abdc3bac223d04347e0
tolkunasanaly <tolkusha@gmail.com>
2020-03-31 19:12:12 -0400

    tests

--- src/test/java/com/automation/pages/activities/CalendarEventsPage.java
+++ src/test/java/com/automation/pages/activities/CalendarEventsPage.java
@@ -19,23 +19,39 @@ public class CalendarEventsPage extends AbstractPageBase {
     @FindBy(css = "[id^='date_selector_oro_calendar_event_form_start']")
     private WebElement startDate;
 
+    @FindBy(css = "[id^='time_selector_oro_calendar_event_form_start']")
+    private WebElement startTime;
 
-    public String getOwnerName(){
-        BrowserUtils.waitForPageToLoad(10);
+    @FindBy(css = "[id^='time_selector_oro_calendar_event_form_end']")
+    private WebElement endTime;
+
+    public String getStartTime() {
+        BrowserUtils.waitForPageToLoad(20);
+        return startTime.getAttribute("value");
+    }
+
+    public String getEndTime() {
+        BrowserUtils.waitForPageToLoad(20);
+        return endTime.getAttribute("value");
+    }
+
+    public String getOwnerName() {
+        BrowserUtils.waitForPageToLoad(20);
         //wait for element to be present in DOM
         wait.until(ExpectedConditions.presenceOfElementLocated(By.className("select2-chosen")));
         wait.until(ExpectedConditions.visibilityOf(owner));
         return owner.getText().trim();
     }
 
-    public void clickToCreateCalendarEvent(){
-        BrowserUtils.waitForPageToLoad(10);
+    public void clickToCreateCalendarEvent() {
+        BrowserUtils.waitForPageToLoad(20);
         wait.until(ExpectedConditions.elementToBeClickable(createCalendarEvent)).click();
     }
 
-    public String getStartDate(){
-        BrowserUtils.waitForPageToLoad(10);
+    public String getStartDate() {
+        BrowserUtils.waitForPageToLoad(20);
         wait.until(ExpectedConditions.visibilityOf(startDate));
+        BrowserUtils.scrollTo(startDate);
         return startDate.getAttribute("value");
     }
 }
--- src/test/java/com/automation/utilities/DateTimeUtilities.java
+++ src/test/java/com/automation/utilities/DateTimeUtilities.java
@@ -1,7 +1,9 @@
 package com.automation.utilities;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class DateTimeUtilities {
 
@@ -23,5 +25,18 @@ public class DateTimeUtilities {
      */
     public static String getCurrentDate(String format){
         return LocalDate.now().format(DateTimeFormatter.ofPattern(format));
+    }
+
+    /**
+     * This method returns difference between end and start time
+     * @param start time
+     * @param end time
+     * @param format like h:m a
+     * @return difference between end time and start time as a long
+     */
+    public static long getTimeDifference(String start, String end, String format){
+        LocalTime startTime = LocalTime.parse(start, DateTimeFormatter.ofPattern(format));
+        LocalTime endTime = LocalTime.parse(end, DateTimeFormatter.ofPattern(format));
+        return ChronoUnit.HOURS.between(startTime, endTime);
     }
 }
